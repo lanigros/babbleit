667fdcc1d1696f44dcffef48b497a748614aa369
Theodor-Bjornslatt <77094997+Theodor-Bjornslatt@users.noreply.github.com>
2022-04-08 00:26:22 +0200

    Added error handling to app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
-import express, { Request, Response, Express } from 'express'
+import express, { Request, Response, Express, NextFunction } from 'express'
 import session, { Store } from 'express-session'
 
-import { exampleRoutes } from './api/routes'
+import { authRoutes } from './api/routes'
 import { SESSION_OPTIONS } from './configuration'
 
 export function createApp(store: Store): Express {
@@ -11,11 +11,23 @@ export function createApp(store: Store): Express {
   app.use(session({ ...SESSION_OPTIONS, store }))
 
   //TODO add api routes
-  app.use('/api', exampleRoutes)
+  app.use('/api', authRoutes)
 
   app.use((_: Request, res: Response) => {
     res.status(404).json({ error: 'not found' })
   })
+
+  app.use(
+    (
+      error: Error & { status: number },
+      _: Request,
+      res: Response,
+      next: NextFunction
+    ) => {
+      console.error(error)
+      res.status(error.status || 500).json({ error: error.message })
+    }
+  )
 
   //TODO add error handling here
 
