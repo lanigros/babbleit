bf091f768f2505be9c24630c2c7670595bbdd829
Tobias Gonzalez <62892165+lanigros@users.noreply.github.com>
2022-05-03 16:42:38 +0200

    Added regex to post validation clientside

--- client/validation/postValidation.ts
+++ client/validation/postValidation.ts
@@ -1,14 +1,14 @@
 import { PostCreation } from '../types'
 
 export function validatePostTitle({ title }: Pick<PostCreation, 'title'>) {
-  const titleRegex = /^([,. a-zA-Z0-9äöåÄÖÅ]){10,40}$/
+  const titleRegex = /^([,\. a-zA-Z0-9äöåÄÖÅ@$!%*?\-_&]+){10,40}$/
   return titleRegex.test(title)
 }
 
 export function validatePostContent({
   content
 }: Pick<PostCreation, 'content'>) {
-  const titleRegex = /^([,. a-zA-Z0-9äöåÄÖÅ@$!%*?-_&]){10,1000}$/
+  const titleRegex = /^([,\. a-zA-Z0-9äöåÄÖÅ@$!%*?\-_&]+){10,1000}$/
   return titleRegex.test(content)
 }
 
@@ -20,12 +20,12 @@ export function validateCommunityPost(post: PostCreation) {
 
   if (!isTitleValid) {
     invalidValues.title =
-      "Title must be 10 - 40 characters long and can't contain any special characters."
+      'Title must be 10 - 40 characters long can contain the following special characters: @ $ ! % * ? , . - _ & Å Ä Ö'
   }
 
   if (!isContentValid) {
     invalidValues.content =
-      'Content must be 10 - 1000 characters long and can contain the following special characters: @ $ ! % * ? - _ & Å Ä Ö'
+      'Content must be 10 - 1000 characters long and can contain the following special characters: @ $ ! % * ? , . - _ & Å Ä Ö'
   }
 
   return invalidValues
